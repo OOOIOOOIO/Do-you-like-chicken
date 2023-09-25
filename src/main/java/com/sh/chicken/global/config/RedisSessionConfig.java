b16454b3609d68c3c2b8a16b18beb6f567da45c8
OOOIOOOIO <polite159@gmail.com>
2023-09-25 14:18:15 +0900

    feat: swagger 적용 및 예외처리 적용

--- src/main/java/com/sh/chicken/global/config/RedisSessionConfig.java
+++ src/main/java/com/sh/chicken/global/config/RedisSessionConfig.java
@@ -1,9 +1,17 @@
 package com.sh.chicken.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession
 public class RedisSessionConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory();
+    }
 }
--- src/main/java/com/sh/chicken/global/exception/ErrorCode.java
+++ src/main/java/com/sh/chicken/global/exception/ErrorCode.java
@@ -13,7 +13,11 @@ public enum ErrorCode {
 
     NOT_FOUND_USER(BAD_REQUEST, "회원 정보가 올바르지 않습니다."),
     ALREADY_EXIST_EMAIL(BAD_REQUEST, "이미 존재하는 이메일입니다."),
-    S3_FILE_UPLOAD(BAD_REQUEST, "S3 file upload에 실패하였습니다.");
+    S3_FILE_UPLOAD(BAD_REQUEST, "S3 file upload에 실패하였습니다."),
+    FAIL_SAVE_IN_REDIS(BAD_REQUEST, "redis에 데이터 저장에 실패하였습니다."),
+    FAIL_DELETE_FROM_REDIS(BAD_REQUEST, "redis에서 데이터 삭제에 실패하였습니다."),
+    ALREADY_SAVE(BAD_REQUEST, "중복 저장을 시도하였습니다."),
+    ALREADY_DELETE(BAD_REQUEST, "중복 삭제를 시도하였습니다."),;
 
 
     private final HttpStatus httpStatus;
